e2c09019a015b8fe2fd0a06f047f70d0f93b6839
Gustaf Andersson <73471364+gustaf87andersson@users.noreply.github.com>
2021-04-14 09:25:41 +0200

    Add Student Entity (#3)
    Co-authored-by: GETIT-GUSTAF\Gustaf Andersson <ganders9@volvocars.com>

---
+++ springdemo/src/main/java/com/example/entities/Student.java
@@ -0,0 +1,43 @@
+package com.example.entities;
+
+public class Student {
+
+	public Student() {		
+	}
+	
+	public Student(int id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}	
+	
+}
