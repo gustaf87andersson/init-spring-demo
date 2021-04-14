3545aa1d4e05eb8bc0e9436087649305a95905dc
GETIT-GUSTAF\Gustaf Andersson <ganders9@volvocars.com>
2021-04-14 10:06:54 +0200

    Add StudentsController and and ComponentScan

--- springdemo/src/main/java/com/example/springdemo/SpringdemoApplication.java
+++ springdemo/src/main/java/com/example/springdemo/SpringdemoApplication.java
@@ -2,8 +2,10 @@ package com.example.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.example.controllers"})
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
