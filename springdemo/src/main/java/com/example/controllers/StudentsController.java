1b234d1d60d41a746558f4f6083b9b7ac80445c8
GETIT-GUSTAF\Gustaf Andersson <ganders9@volvocars.com>
2021-04-20 11:33:05 +0200

    Add regex path selector with Regex

--- springdemo/src/main/java/com/example/controllers/StudentsController.java
+++ springdemo/src/main/java/com/example/controllers/StudentsController.java
@@ -1,8 +1,6 @@
 package com.example.controllers;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- springdemo/src/main/java/com/example/springdemo/SpringdemoApplication.java
+++ springdemo/src/main/java/com/example/springdemo/SpringdemoApplication.java
@@ -13,6 +13,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -28,6 +29,7 @@ public class SpringdemoApplication {
 		// Returns a prepared Docket instance
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
+				.paths(regex("/api.*"))
 				.apis(RequestHandlerSelectors.basePackage("com.example"))
 				.build()
 				.apiInfo(apiDetails());
