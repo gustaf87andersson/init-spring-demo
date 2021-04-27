66a4ade2d36013c0819c318c1815091d2026dccd
Gustaf Andersson <73471364+gustaf87andersson@users.noreply.github.com>
2021-04-27 17:55:58 +0200

    lombok (#8)
    Co-authored-by: GETIT-GUSTAF\Gustaf Andersson <ganders9@volvocars.com>

--- springdemo/src/main/java/com/example/entities/Student.java
+++ springdemo/src/main/java/com/example/entities/Student.java
@@ -5,8 +5,12 @@ import java.util.UUID;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 @ApiModel(description = "Add new Student request model")
+@Getter
+@Setter
 public class Student {
 		
 	@ApiModelProperty(notes = "The unique identifier of an Student")
@@ -19,46 +23,5 @@ public class Student {
 	private Date createdAt;
 	@ApiModelProperty(notes = "The date the student was last updated")
 	private Date updatedAt;
-	
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
 	
 }
