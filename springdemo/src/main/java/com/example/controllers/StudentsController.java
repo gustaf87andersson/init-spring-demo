5b95be2a6d16a342a74f7c4bf95c3d4292e66057
GETIT-GUSTAF\Gustaf Andersson <ganders9@volvocars.com>
2021-05-03 10:02:24 +0200

    Add controller errorhandling

--- springdemo/src/main/java/com/example/controllers/StudentsController.java
+++ springdemo/src/main/java/com/example/controllers/StudentsController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.example.entities.*;
 import com.example.requests.AddStudentRequest;
 import com.example.requests.UpdateStudentRequest;
@@ -26,36 +29,41 @@ public class StudentsController {
 
 	@Autowired
 	private StudentService studentService;
-	
-	
+
 	@GetMapping("/")
-	@ApiOperation(
-			value = "Get all Students",
-			notes = "Fetches all students in Students API",
-			response = Student.class,
-			responseContainer = "List")
-	public Collection<Student> getAll() {		
-		return studentService.getAll();		
+	@ApiOperation(value = "Get all Students", notes = "Fetches all students in Students API", response = Student.class, responseContainer = "List")
+	public Collection<Student> getAll() {
+		return studentService.getAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public Student getById(@ApiParam(value = "Id of the Student", required = true) @PathVariable UUID id) {
-		return studentService.getById(id);
+		var response = studentService.getById(id);
+
+		if (response == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldnt find any student with provided ID");
+		}
+
+		return response;
 	}
-	
+
 	@PostMapping("/")
-	public Student addNewStudent(@RequestBody AddStudentRequest request) {		
+	public Student addNewStudent(@RequestBody AddStudentRequest request) {
+		if (request.getFirstName().isEmpty() || request.getLastName().isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Firstname and Lastname are manadatory");
+		}
+
 		return studentService.addStudent(request);
 	}
-	
+
 	@PutMapping("/{id}")
-	public Student updateStudent(@PathVariable UUID id, @RequestBody UpdateStudentRequest request) {		
+	public Student updateStudent(@PathVariable UUID id, @RequestBody UpdateStudentRequest request) {
 		return studentService.updateStudent(id, request);
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public void deleteStudent(@PathVariable UUID id) {
 		studentService.deleteStudent(id);
 	}
-	
+
 }
