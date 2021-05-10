8c1cc68c2b5cd2c39643a38fa10921e4f8562876
Gustaf Andersson <gustaf.andersson@getitnordic.se>
2021-05-10 09:39:33 +0200

    add logging and aop example

--- springdemo/infrastructure/sql/CREATE_TABLES.sql
+++ springdemo/infrastructure/sql/CREATE_TABLES.sql
@@ -1,12 +1,22 @@
+CREATE TABLE Address (
+	id int IDENTITY(1,1) PRIMARY KEY,
+	street varchar(255) NOT NULL,
+	city varchar(255) NOT NULL
+);
 
--- DROP TABLE Student
-
+-- STUDENT TABLE
 CREATE TABLE Student (
 	id int IDENTITY(1,1) PRIMARY KEY,
 	first_name varchar(255) NOT NULL,
 	last_name varchar(255) NOT NULL,
 	created_at datetime NOT NULL,
 	updated_at datetime NULL,
+	address_id int FOREIGN KEY REFERENCES Address(id)
 );
 
-
+-- COURSE TABLE
+CREATE TABLE Course (
+	id int IDENTITY(1,1) PRIMARY KEY,
+	name varchar(255) NOT NULL,
+	student_id int FOREIGN KEY REFERENCES Student(id)
+);
